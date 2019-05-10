691e6b4c694fe255311d621fe5f35dd03ad1cb3f
shiv pratap singh <pratapshiv1992@gmail.com>
2019-05-11 02:57:17 +0530

    basic routing implemented

--- src/App.js
+++ src/App.js
@@ -1,6 +1,34 @@
 import React from 'react';
-const App = ()=><h1>Basic react App</h1>
+import { 
+    Route,
+    Link,
+    Switch,
+    Redirect,
+} from 'react-router-dom';
+
+const App = () => (
+    <>
+      <Link to="/">Invoice Listing</Link>
+      <Link to="add-invoice">Add Invoice</Link>
+      <Link to="about-developer">About Developer</Link>
+      <Switch>
+        <Route path="/" exact componet={<>This is invoice listing. </>} />
+        <Route
+          path="/add-invoice"
+          exact
+          componet={() => <>This is add invoice . </>}
+        />
+        <Route
+          path="/about-developer"
+          exact
+          componet={() => <>My introduction. </>}
+        />
+        <Redirect to="/" />
+      </Switch>
+    </>
+  );
+  
+  
+
+
 export default App;
-
-
-
